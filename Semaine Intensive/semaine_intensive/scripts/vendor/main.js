b5c5c9258e87075ee086d1a56d60e4541aa756d7
baptisteanorga <baptiste.anorga@yahoo.fr>
2017-02-02 00:49:18 +0100

    jss à jour sur page produits

--- Semaine Intensive/semaine_intensive/scripts/vendor/main.js
+++ Semaine Intensive/semaine_intensive/scripts/vendor/main.js
@@ -10,8 +10,15 @@ document.querySelector(".plus_1").onclick = function () {
 		{
 			tabPlus[i].style.display="block";
 		}
+		var tabCat = document.querySelectorAll(".categorie");
+		for( i=0 ; i < tabRond.length ; i++)
+		{
+			tabCat[i].style.display="block";
+		}
 		document.querySelector(".rond_1").style.display="block";
 		document.querySelector(".plus_1").style.display="none";
+		document.querySelector(".categorie_1").style.display="none";
+
 	}
 }
 document.querySelector(".plus_2").onclick = function() {
@@ -26,8 +33,14 @@ document.querySelector(".plus_2").onclick = function() {
 		{
 			tabPlus[i].style.display="block";
 		}
+		var tabCat = document.querySelectorAll(".categorie");
+		for( i=0 ; i < tabRond.length ; i++)
+		{
+			tabCat[i].style.display="block";
+		}
 		document.querySelector(".rond_2").style.display="block";
 		document.querySelector(".plus_2").style.display="none";
+		document.querySelector(".categorie_2").style.display="none";
 	}
 }
 
@@ -43,8 +56,14 @@ document.querySelector(".plus_3").onclick = function () {
 		{
 			tabPlus[i].style.display="block";
 		}
+		var tabCat = document.querySelectorAll(".categorie");
+		for( i=0 ; i < tabRond.length ; i++)
+		{
+			tabCat[i].style.display="block";
+		}
 		document.querySelector(".rond_3").style.display="block";
 		document.querySelector(".plus_3").style.display="none";
+		document.querySelector(".categorie_3").style.display="none";
 	}
 }
 document.querySelector(".plus_4").onclick = function() {
@@ -59,24 +78,14 @@ document.querySelector(".plus_4").onclick = function() {
 		{
 			tabPlus[i].style.display="block";
 		}
-		document.querySelector(".rond_4").style.display="block";
-		document.querySelector(".plus_4").style.display="none";
-	}
-}
-document.querySelector(".plus_4").onclick = function() {
-	if (window.getComputedStyle(document.querySelector('.rond_4')).display =='none'){
-		var tabRond = document.querySelectorAll(".rond");
+		var tabCat = document.querySelectorAll(".categorie");
 		for( i=0 ; i < tabRond.length ; i++)
 		{
-			tabRond[i].style.display="none";
-		}
-		var tabPlus = document.querySelectorAll(".plus");
-		for( i=0 ; i < tabRond.length ; i++)
-		{
-			tabPlus[i].style.display="block";
+			tabCat[i].style.display="block";
 		}
 		document.querySelector(".rond_4").style.display="block";
 		document.querySelector(".plus_4").style.display="none";
+		document.querySelector(".categorie_4").style.display="none";
 	}
 }
 document.querySelector(".plus_5").onclick = function() {
@@ -91,8 +100,14 @@ document.querySelector(".plus_5").onclick = function() {
 		{
 			tabPlus[i].style.display="block";
 		}
+		var tabCat = document.querySelectorAll(".categorie");
+		for( i=0 ; i < tabRond.length ; i++)
+		{
+			tabCat[i].style.display="block";
+		}
 		document.querySelector(".rond_5").style.display="block";
 		document.querySelector(".plus_5").style.display="none";
+		document.querySelector(".categorie_5").style.display="none";
 	}
 }
 document.querySelector(".plus_6").onclick = function() {
@@ -107,8 +122,14 @@ document.querySelector(".plus_6").onclick = function() {
 		{
 			tabPlus[i].style.display="block";
 		}
+		var tabCat = document.querySelectorAll(".categorie");
+		for( i=0 ; i < tabRond.length ; i++)
+		{
+			tabCat[i].style.display="block";
+		}
 		document.querySelector(".rond_6").style.display="block";
 		document.querySelector(".plus_6").style.display="none";
+		document.querySelector(".categorie_6").style.display="none";
 	}
 }
 document.querySelector(".plus_7").onclick = function() {
@@ -123,8 +144,14 @@ document.querySelector(".plus_7").onclick = function() {
 		{
 			tabPlus[i].style.display="block";
 		}
+		var tabCat = document.querySelectorAll(".categorie");
+		for( i=0 ; i < tabRond.length ; i++)
+		{
+			tabCat[i].style.display="block";
+		}
 		document.querySelector(".rond_7").style.display="block";
 		document.querySelector(".plus_7").style.display="none";
+		document.querySelector(".categorie_7").style.display="none";
 	}
 }
 document.querySelector(".plus_8").onclick = function() {
@@ -139,8 +166,14 @@ document.querySelector(".plus_8").onclick = function() {
 		{
 			tabPlus[i].style.display="block";
 		}
+		var tabCat = document.querySelectorAll(".categorie");
+		for( i=0 ; i < tabRond.length ; i++)
+		{
+			tabCat[i].style.display="block";
+		}
 		document.querySelector(".rond_8").style.display="block";
 		document.querySelector(".plus_8").style.display="none";
+		document.querySelector(".categorie_8").style.display="none";
 	}
 }
 document.querySelector(".plus_9").onclick = function() {
@@ -155,61 +188,77 @@ document.querySelector(".plus_9").onclick = function() {
 		{
 			tabPlus[i].style.display="block";
 		}
+		var tabCat = document.querySelectorAll(".categorie");
+		for( i=0 ; i < tabRond.length ; i++)
+		{
+			tabCat[i].style.display="block";
+		}
 		document.querySelector(".rond_9").style.display="block";
 		document.querySelector(".plus_9").style.display="none";
+		document.querySelector(".categorie_9").style.display="none";
 	}
 }
 document.querySelector(".croix_1").onclick = function() {
 	if (window.getComputedStyle(document.querySelector('.rond_1')).display =='block'){
 		document.querySelector(".rond_1").style.display="none";
 		document.querySelector(".plus_1").style.display="block";
+		document.querySelector(".categorie_1").style.display="block";
+		
 	}
 }
 document.querySelector(".croix_2").onclick = function() {
 	if (window.getComputedStyle(document.querySelector('.rond_2')).display =='block'){
 		document.querySelector(".rond_2").style.display="none";
 		document.querySelector(".plus_2").style.display="block";
+		document.querySelector(".categorie_2").style.display="block";
 	}
 }
 document.querySelector(".croix_3").onclick = function() {
 	if (window.getComputedStyle(document.querySelector('.rond_3')).display =='block'){
 		document.querySelector(".rond_3").style.display="none";
 		document.querySelector(".plus_3").style.display="block";
+		document.querySelector(".categorie_3").style.display="block";
 	}
 }
 document.querySelector(".croix_4").onclick = function() {
 	if (window.getComputedStyle(document.querySelector('.rond_4')).display =='block'){
 		document.querySelector(".rond_4").style.display="none";
 		document.querySelector(".plus_4").style.display="block";
+		document.querySelector(".categorie_4").style.display="block";
 	}
 }
 document.querySelector(".croix_5").onclick = function() {
 	if (window.getComputedStyle(document.querySelector('.rond_5')).display =='block'){
 		document.querySelector(".rond_5").style.display="none";
 		document.querySelector(".plus_5").style.display="block";
+		document.querySelector(".categorie_5").style.display="block";
 	}
 }
 document.querySelector(".croix_6").onclick = function() {
 	if (window.getComputedStyle(document.querySelector('.rond_6')).display =='block'){
 		document.querySelector(".rond_6").style.display="none";
 		document.querySelector(".plus_6").style.display="block";
+		document.querySelector(".categorie_6").style.display="block";
 	}
 }
 document.querySelector(".croix_7").onclick = function() {
 	if (window.getComputedStyle(document.querySelector('.rond_7')).display =='block'){
-		document.querySelector(".rond_7").style.display="none";
-		document.querySelector(".plus_7").style.display="block";
+		document.querySelector(".rond_7").style.display = "none";
+		document.querySelector(".plus_7").style.display = "block";
+		document.querySelector(".categorie_7").style.display = "block";
 	}
 }
 document.querySelector(".croix_8").onclick = function() {
 	if (window.getComputedStyle(document.querySelector('.rond_8')).display =='block'){
 		document.querySelector(".rond_8").style.display="none";
 		document.querySelector(".plus_8").style.display="block";
+		document.querySelector(".categorie_8").style.display="block";
 	}
 }
 document.querySelector(".croix_9").onclick = function() {
 	if (window.getComputedStyle(document.querySelector('.rond_9')).display =='block'){
 		document.querySelector(".rond_9").style.display="none";
 		document.querySelector(".plus_9").style.display="block";
+		document.querySelector(".categorie_9").style.display="block";
 	}
 }
