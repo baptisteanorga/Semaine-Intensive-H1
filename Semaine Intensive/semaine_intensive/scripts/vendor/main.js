b432496ce4d9418da0d7d55a6ce58370a5423aa5
baptisteanorga <baptiste.anorga@yahoo.fr>
2017-02-01 10:36:02 +0100

    ajout page js blabla

---
+++ Semaine Intensive/semaine_intensive/scripts/vendor/main.js
@@ -0,0 +1,99 @@
+document.querySelector(".produit1").onclick = function() {
+  if (window.getComputedStyle(document.querySelector('.cultureText')).display=='none'){
+    document.querySelector(".cultureText").style.display="block";
+    document.querySelector(".cultureText1").style.display="none";
+    document.querySelector(".cultureText2").style.display="none";
+    document.querySelector(".cultureText3").style.display="none";
+    document.querySelector(".cultureText4").style.display="none";
+  } else {
+    document.querySelector(".cultureText").style.display="none";
+  }
+}
+document.querySelector(".produit2").onclick = function() {
+  if (window.getComputedStyle(document.querySelector('.cultureText1')).display=='none'){
+    document.querySelector(".cultureText1").style.display="block";
+    document.querySelector(".cultureText2").style.display="none";
+    document.querySelector(".cultureText3").style.display="none";
+    document.querySelector(".cultureText4").style.display="none";
+    document.querySelector(".cultureText").style.display="none";
+  } else {
+    document.querySelector(".cultureText1").style.display="none";
+  }
+}
+document.querySelector(".produit3").onclick = function() {
+  if (window.getComputedStyle(document.querySelector('.cultureText2')).display=='none'){
+    document.querySelector(".cultureText2").style.display="block";
+    document.querySelector(".cultureText1").style.display="none";
+    document.querySelector(".cultureText3").style.display="none";
+    document.querySelector(".cultureText4").style.display="none";
+    document.querySelector(".cultureText").style.display="none";
+  } else {
+    document.querySelector(".cultureText2").style.display="none";
+  }
+}
+document.querySelector(".produit4").onclick = function() {
+  if (window.getComputedStyle(document.querySelector('.cultureText3')).display=='none'){
+    document.querySelector(".cultureText3").style.display="block";
+    document.querySelector(".cultureText2").style.display="none";
+    document.querySelector(".cultureText1").style.display="none";
+    document.querySelector(".cultureText4").style.display="none";
+    document.querySelector(".cultureText").style.display="none";
+  } else {
+    document.querySelector(".cultureText3").style.display="none";
+  }
+}
+document.querySelector(".produit5").onclick = function() {
+  if (window.getComputedStyle(document.querySelector('.cultureText4')).display=='none'){
+    document.querySelector(".cultureText4").style.display="block";
+    document.querySelector(".cultureText2").style.display="none";
+    document.querySelector(".cultureText3").style.display="none";
+    document.querySelector(".cultureText1").style.display="none";
+    document.querySelector(".cultureText").style.display="none";
+  } else {
+    document.querySelector(".cultureText6").style.display="none";
+  }
+}
+document.querySelector(".produit6").onclick = function() {
+  if (window.getComputedStyle(document.querySelector('.cultureText4')).display=='none'){
+    document.querySelector(".cultureText4").style.display="block";
+    document.querySelector(".cultureText2").style.display="none";
+    document.querySelector(".cultureText3").style.display="none";
+    document.querySelector(".cultureText1").style.display="none";
+    document.querySelector(".cultureText").style.display="none";
+  } else {
+    document.querySelector(".cultureText6").style.display="none";
+  }
+}
+document.querySelector(".produit7").onclick = function() {
+  if (window.getComputedStyle(document.querySelector('.cultureText4')).display=='none'){
+    document.querySelector(".cultureText4").style.display="block";
+    document.querySelector(".cultureText2").style.display="none";
+    document.querySelector(".cultureText3").style.display="none";
+    document.querySelector(".cultureText1").style.display="none";
+    document.querySelector(".cultureText").style.display="none";
+  } else {
+    document.querySelector(".cultureText6").style.display="none";
+  }
+}
+document.querySelector(".produit8").onclick = function() {
+  if (window.getComputedStyle(document.querySelector('.cultureText4')).display=='none'){
+    document.querySelector(".cultureText4").style.display="block";
+    document.querySelector(".cultureText2").style.display="none";
+    document.querySelector(".cultureText3").style.display="none";
+    document.querySelector(".cultureText1").style.display="none";
+    document.querySelector(".cultureText").style.display="none";
+  } else {
+    document.querySelector(".cultureText6").style.display="none";
+  }
+}
+document.querySelector(".produit9").onclick = function() {
+  if (window.getComputedStyle(document.querySelector('.cultureText4')).display=='none'){
+    document.querySelector(".cultureText4").style.display="block";
+    document.querySelector(".cultureText2").style.display="none";
+    document.querySelector(".cultureText3").style.display="none";
+    document.querySelector(".cultureText1").style.display="none";
+    document.querySelector(".cultureText").style.display="none";
+  } else {
+    document.querySelector(".cultureText6").style.display="none";
+  }
+}
